34509d0fe4d5b52de2b09b0644497a34319d44d4
Jack Farrand <jack.farand@gmail.com>
2017-02-04 15:00:50 +0000

    added timer update logic, prior to AJAX requests

--- timer.js
+++ timer.js
@@ -1,4 +1,28 @@
 function loadTimers() 
 {
-	alert("loading timers");
+	window.setInterval(updateTimers, 1000);
 }
+
+function updateTimers() 
+{
+	updateCountDownTimer();
+	updateCountUpTimer();
+}
+
+function updateCountDownTimer() 
+{
+	var d = new Date();
+   document.getElementById("timer_down").innerHTML = d.toLocaleTimeString();
+}
+
+function updateCountUpTimer() 
+{
+	var d = new Date();
+   document.getElementById("timer_up").innerHTML = d.toLocaleTimeString();
+}
+
+function getMessages() 
+{
+	//Todo, display messages from host.
+}
+
