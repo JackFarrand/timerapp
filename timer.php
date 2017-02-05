302de0e9bcf44519011dba2521df615c90e0fef9
Jack Farrand <jack.farand@gmail.com>
2017-02-05 19:29:24 +0000

    updating time operations in php, after 500 error

--- timer.php
+++ timer.php
@@ -2,6 +2,6 @@
 
 $now = new DateTime();
 echo $now->format('Y-m-d H:i:s');    // MySQL datetime format
-echo $now->getTimestamp();           // Unix Timestamp -- Since PHP 5.3
+//echo $now->getTimestamp();           // Unix Timestamp -- Since PHP 5.3
 
 ?>
