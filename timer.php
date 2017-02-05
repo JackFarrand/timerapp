33848afcbe420045dc764b003d2043c3635b9798
Jack Farrand <jack.farand@gmail.com>
2017-02-05 21:30:40 +0000

    successfully implemented APC in PHP [I yum installed the php-pecl-apcu package and restarted httpd] and added comments to the server-side code so I might actually be able to read it in the future.

--- timer.php
+++ timer.php
@@ -1,25 +1,27 @@
+//Copyright Jack Farrand 2017
 <?php
 
-ini_set('display_errors', 'On');
+ini_set('display_errors', 'On');  //enable error reporting while I'm debugging the code.
 error_reporting(E_ALL);
 
 function setTimer() 
 {
-	echo "Timer Successfully Set";
+	echo "Timer Successfully Set"; //Dummy code while I set it all up
 }
 
 function getTalkName() 
 {
-	//$talkName = "Treasures from God's Word";
-	$talkName = apc_fetch('talkName');
+	$talkName = apc_fetch('talkName'); //retrieve this from the APC short term server cache, persistent between script calls.
 	echo $talkName;
 }
 
 function setTalkName() 
 {
 	$talkName = $_GET['talkName'];
-	apc_store("talkName", $talkName);
+	apc_store("talkName", $talkName); //store this in the APC cache, short term server-side storage so we can get at it later
 }
+
+/*Check the http header for function calls and execute the relevant functions if required*/
 
 if($_GET['functioncall'] === "setTimer") 
 {
@@ -34,10 +36,12 @@ elseif($_GET['functioncall'] === "setTalkName")
 	setTalkName();
 }
 
+/*if an invalid function call is sent, reply with a stupid message*/
 else 
 {
 	echo "Hello World\n";
 	echo date('m/d/Y h:i:s a', time());
 	echo "pick a function call next time, dingus.";
 }
+
 ?>
