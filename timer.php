2ec5e09ec4f666fde40c6b2b40e0a5b8b179815b
Jack Farrand <jack.farand@gmail.com>
2017-02-05 19:30:15 +0000

    reverting to older php code after server errors

--- timer.php
+++ timer.php
@@ -1,7 +1,7 @@
 <?php
 
-$now = new DateTime();
-echo $now->format('Y-m-d H:i:s');    // MySQL datetime format
-//echo $now->getTimestamp();           // Unix Timestamp -- Since PHP 5.3
+echo "Hello World";
+
+echo date('m/d/Y h:i:s a', time());
 
 ?>
