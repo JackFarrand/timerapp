4e32b78c317df726f7ee390705a77f5dd7652a97
Jack Farrand <jack.farrand@gmail.com>
2017-02-06 23:19:16 +0000

    fixed spelling error bug in php, this is why I hate scripting

--- timer.php
+++ timer.php
@@ -26,7 +26,7 @@ function getMessages()
 function sendMessage() 
 {
 	$messages = apc_fetch('messages');
-	$messages .= "<p>" . santize($_GET['message']) . "</p>";  //sanitize the input we receive from the client
+	$messages .= "<p>" . sanitize($_GET['message']) . "</p>";  //sanitize the input we receive from the client
 	apc_store("messages", $messages);
 }
 
