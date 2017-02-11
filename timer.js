a6aa604b446af31c450a3c5a38ad6ef84d815d78
Jack Farrand <jack.farand@gmail.com>
2017-02-11 22:14:53 +0000

    added fullscreen button

--- timer.js
+++ timer.js
@@ -25,10 +25,46 @@ var talkLengths =
   		<option>Closing Review & Preview</option>
   	</select> */
 
+ function toggleFullScreen(elem)
+{
+if ((document.fullScreenElement !== undefined && document.fullScreenElement === null) 
+        || (document.msFullscreenElement !== undefined && document.msFullscreenElement === null) 
+        || (document.mozFullScreen !== undefined && !document.mozFullScreen) 
+        || (document.webkitIsFullScreen !== undefined && !document.webkitIsFullScreen)) 
+    {
+                if (elem.requestFullScreen) {
+                    elem.requestFullScreen();
+                } else if (elem.mozRequestFullScreen) {
+                    elem.mozRequestFullScreen();
+                } else if (elem.webkitRequestFullScreen) {
+                    elem.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+                } else if (elem.msRequestFullscreen) {
+                    elem.msRequestFullscreen();
+                }
+            } else {
+                if (document.cancelFullScreen) {
+                    document.cancelFullScreen();
+                } else if (document.mozCancelFullScreen) {
+                    document.mozCancelFullScreen();
+                } else if (document.webkitCancelFullScreen) {
+                    document.webkitCancelFullScreen();
+                } else if (document.msExitFullscreen) {
+                    document.msExitFullscreen();
+                }
+            }
+        }
+
+
+function Fullscreen()
+{
+	toggleFullScreen(document.documentElement);
+}
+
 function loadTimers() 
 {
 	window.setInterval(updateTimers, 1000);	
 }
+
 
 function updateTimers() 
 {
